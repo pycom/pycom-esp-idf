f20cf85864dafa52d3f27ee04f6b4510fa44a38e
Christian Ehlers <cehlers@pycom.io>
2019-01-10 19:26:02 +0100

    Enabled LCP echo intervals

--- components/lwip/include/lwip/lwip/opt.h
+++ components/lwip/include/lwip/lwip/opt.h
@@ -2275,7 +2275,7 @@
  * LCP_ECHOINTERVAL: Interval in seconds between keepalive echo requests, 0 to disable.
  */
 #ifndef LCP_ECHOINTERVAL
-#define LCP_ECHOINTERVAL                0
+#define LCP_ECHOINTERVAL                10
 #endif
 
 /**
