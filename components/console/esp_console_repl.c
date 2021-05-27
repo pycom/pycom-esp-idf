070500621497c44d1512cde85426d1e4fb269875
Geza Husi <geza@pycom.io>
2021-05-27 21:34:27 +0200

    Fix compilation issues

--- components/console/esp_console_repl.c
+++ components/console/esp_console_repl.c
@@ -96,7 +96,7 @@ esp_err_t esp_console_new_repl_uart(const esp_console_dev_uart_config_t *dev_con
     uart_set_pin(dev_config->channel, dev_config->tx_gpio_num, dev_config->rx_gpio_num, -1, -1);
 
     /* Install UART driver for interrupt-driven reads and writes */
-    ret = uart_driver_install(dev_config->channel, 256, 0, 0, NULL, 0);
+    ret = uart_driver_install(dev_config->channel, 256, 0, 0, NULL, 0, NULL);
     if (ret != ESP_OK) {
         goto _exit;
     }
